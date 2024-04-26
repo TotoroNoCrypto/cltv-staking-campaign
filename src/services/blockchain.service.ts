c203d2602dd8b2be17a4dee392a8b1d34182422a
TotoroNoCrypto <totoronocrypto@gmail.com>
2024-04-26 21:12:37 +0200

    Add bitcoin-simple-rpc

--- src/services/blockchain.service.ts
+++ src/services/blockchain.service.ts
@@ -1,5 +1,19 @@
+import { Client } from 'bitcoin-simple-rpc'
+import config from 'config'
+
+const bitcoinUrl = config.get<string>('bitcoin.url')
+const bitcoinUsername = config.get<string>('bitcoin.username')
+const bitcoinPassword = config.get<string>('bitcoin.password')
+
 export class BlockchainService {
-  public static async stake(txHex: string): Promise<string> {
+    public static async stake(txHex: string): Promise<string> {
+        const client = new Client({
+        baseURL: bitcoinUrl,
+        auth: { username: bitcoinUsername, password: bitcoinPassword },
+    })
+    const info = await client.getBlockchainInfo();
+    console.log(`info: ${info.blocks}`)
+
     return ''
   }
 }
