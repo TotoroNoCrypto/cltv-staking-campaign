19d31b853fdd04baaec95f7fb682bf09b9d9933e
TotoroNoCrypto <totoronocrypto@gmail.com>
2024-05-31 12:24:09 +0200

    Add 20% to mempool fees

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ export async function getFastestFee(): Promise<number> {
     } = mempoolJS({ hostname: 'mempool.space' })
     const feesRecmmended = await fees.getFeesRecommended()
 
-    return feesRecmmended.fastestFee
+    return Math.round(feesRecmmended.fastestFee * 1.2)
   }
 
   return 30
